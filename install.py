3da67285c0dac86e84eb3108f7f0ae4f1089541e
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-28 19:10:56 -0500

    Update install.py

--- install.py
+++ install.py
@@ -3,16 +3,16 @@ import os
 
 currentdir=os.getcwd()
 files = [x for x in os.listdir(currentdir+str("/Lists")) if x.endswith('.ltxt')]
-if (os.path.isdir(str(currentdir)+"/ToolBox")) is False:
-	os.makedir(str(currentdir)+"/ToolBox")
+if (os.path.isdir(str(currentdir)+"/Blue_ToolBox")) is False:
+	os.makedir(str(currentdir)+"/Blue_ToolBox")
 os.system("cd "+str(currentdir)+ "/Lists")
 for filename in files:
 	print "--Starting work on "+str(filename)
 	dirname= os.path.splitext(str(filename))[0]
-	os.system("cd "+str(currentdir)+"/ToolBox/ && mkdir "+str(dirname))
+	os.system("cd "+str(currentdir)+"/Blue_ToolBox/ && mkdir "+str(dirname))
 	f=open(str(currentdir)+ "/Lists/"+str(filename))
 	line=f.readline()
 	while line:
-		os.system("cd "+str(currentdir)+"/ToolBox/"+str(dirname) +" && "+line)
+		os.system("cd "+str(currentdir)+"/Blue_ToolBox/"+str(dirname) +" && "+line)
 		line=f.readline()
 	f.close()
