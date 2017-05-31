ec5d83179d38bdef5839746d5fca66cafa9b1e98
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-30 20:16:27 -0500

    Update install.py

--- install.py
+++ install.py
@@ -2,17 +2,20 @@
 import os
 
 currentdir=os.getcwd()
-files = [x for x in os.listdir(currentdir+str("/Lists")) if x.endswith('.ltxt')]
 if (os.path.isdir(str(currentdir)+"/Blue_ToolBox")) is False:
-	os.makedir(str(currentdir)+"/Blue_ToolBox")
-os.system("cd "+str(currentdir)+ "/Lists")
+	os.makedirs(str(currentdir)+"/Blue_ToolBox")
+os.chdir(str(currentdir)+ "/Lists")
+files = [x for x in os.listdir(currentdir+str("/Lists")) if x.endswith('.txt')]
 for filename in files:
-	print "--Starting work on "+str(filename)
 	dirname= os.path.splitext(str(filename))[0]
-	os.system("cd "+str(currentdir)+"/Blue_ToolBox/ && mkdir "+str(dirname))
+	os.chdir(str(currentdir)+"/Blue_ToolBox/")
+	if (os.path.isdir(str(currentdir)+"/Blue_ToolBox/")) is False:
+			os.system("rm -rf "+str(dirname))
+	os.makedirs(str(currentdir)+"/Blue_ToolBox/"+str(dirname))
+	os.chdir(str(currentdir)+"/Blue_ToolBox/"+str(dirname))
 	f=open(str(currentdir)+ "/Lists/"+str(filename))
 	line=f.readline()
 	while line:
-		os.system("cd "+str(currentdir)+"/Blue_ToolBox/"+str(dirname) +" && "+line)
+		os.system(str(line))
 		line=f.readline()
 	f.close()
