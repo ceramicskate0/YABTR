3f35cf2cdfb4fc8e87e88963fa74faa8307593c4
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-09-08 19:56:24 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 function Git ($URI,$CurrentDir)
 {
-	$CurrentDir='C:\Users\Host\Downloads\YABTR-master\YABTR-master'
+	$CurrentDir=(Get-Item -Path ".\").FullName
     $GitName = $URI[0].trimend('/').split('/')[-1] -replace "\.zip$", ''
     #Invoke-WebRequest -Uri $URI+"/archive/master.zip" -OutFile $CurrentDir+"\"+$GitName+"\master.zip"
 	mkdir $CurrentDir"\Windows_Toolbox\"$GitName -force
@@ -9,13 +9,13 @@ function Git ($URI,$CurrentDir)
 
 function DownloadFromWeb ($URI,$CurrentDir)
 {
-	$CurrentDir='C:\Users\Host\Downloads\YABTR-master\YABTR-master'
+	$CurrentDir=(Get-Item -Path ".\").FullName
     $FielName = $URI.trimend('/').split('/')
     (New-Object System.Net.WebClient).DownloadFile($URI[0],$CurrentDir+"\"+$FielName)
 }
 
-foreach($line in Get-Content 'C:\Users\Host\Downloads\YABTR-master\YABTR-master\Lists\Windows.txt') {
-    $CurrentDir='C:\Users\Host\Downloads\YABTR-master\YABTR-master'
+foreach($line in Get-Content (Get-Item -Path ".\").FullName+'Windows.txt') {
+    $CurrentDir=Get-Location
     if ($line -like '*github.com*')
     {
     Git ($line,$CurrentDir)
